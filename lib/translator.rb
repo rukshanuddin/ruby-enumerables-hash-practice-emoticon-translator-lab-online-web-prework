72173a44f6623d202fed82d0de7d7cebfa4be735
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-15 18:09:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,10 @@
 require "yaml"# require modules here
 
-library =YAML.load_file('lib/emoticons.yml')
+
 
 def load_library
-  # code goes here
+  library =YAML.load_file('lib/emoticons.yml')
+
 end
 
 def get_japanese_emoticon
